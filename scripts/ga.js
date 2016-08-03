864a2b4aa960302974bc57dddeafef9ec3dd3115
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-03 22:43:42 +0200

    ga fix

--- scripts/ga.js
+++ scripts/ga.js
@@ -3,14 +3,21 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src='https://www.google-analytics.com/analytics.js';m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-81497637-1', 'auto', {
-   'storage': 'none',
-   'clientId': localStorage.getItem('ga:clientId')
-});
+if (typeof require === 'function') {
+  // electron
+  ga('create', 'UA-81497637-1', 'auto', {
+    'storage': 'none',
+    'clientId': localStorage.getItem('ga:clientId')
+  });
 
-ga(function(tracker) {
-   localStorage.setItem('ga:clientId', tracker.get('clientId'));
-});
+  ga(function(tracker) {
+    localStorage.setItem('ga:clientId', tracker.get('clientId'));
+  });
+
+} else {
+  // classic web site
+  ga('create', 'UA-81497637-1', 'auto');
+}
 
 ga('set', 'checkProtocolTask', null);
 ga("set", "location", "http://necrovisualizer.nicontoso.eu");
